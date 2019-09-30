f1c3840db1ab3899aa57c77066f54414718ca441
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-09-30 14:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names= [ ['Lisa', 'Kaitlin', 'Jan' ], 'surprise' ];
+function writeCards(names, event) {
+
+  for (let i=0; i<names.length; i++){
+   let thanks = [`Thank you, ${names[i]} , for the wonderful surprise gift!`];
+  }
+  return thanks;
+}
+
+
+function countDown (){
+
+  let counter = 10;
+  while (countdown > 10) {
+    console.log(countdown--);
+}
+
+}
