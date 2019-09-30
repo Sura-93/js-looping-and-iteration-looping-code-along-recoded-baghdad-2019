36cbcc5e902b7bedfb987cab531928e56b689abb
Sura Al-jilawi <suraaljailawi@gmail.com>
2019-09-30 14:18:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let names= [['Lisa', 'Kaitlin', 'Jan' ], 'surprise' ];
+function writeCards(names, event) {
+
+  for (let i=0; i<names.length; i++){
+    names [i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  }
+
+  return names;
+}
+
+
+function countdown(num){
+while (num >= 0) {
+    console.log(num--);
+}
+return num;
+}
+countdown(10);
